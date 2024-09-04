b7750179b526127b0ed7018bffef203b61cec976
tranngoctaiutc2 <tranngoct330@gmail.com>
2024-09-04 20:52:07 +0700

    Product Display

--- Project_Demo/Controllers/HomeController.cs
+++ Project_Demo/Controllers/HomeController.cs
@@ -1,21 +1,26 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Project_Demo.Models;
+using Project_Demo.Repository;
 using System.Diagnostics;
 
 namespace Project_Demo.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly DataContext _dataContext;
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, DataContext context)
         {
             _logger = logger;
+            _dataContext = context;
         }
 
         public IActionResult Index()
         {
-            return View();
+            var products = _dataContext.Products.Include("Category").Include("Brand").ToList();
+            return View(products);
         }
 
         public IActionResult Privacy()

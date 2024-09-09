7f7628d9653e5712db4b7b458c6eb40b0715b77e
tranngoctaiutc2 <tranngoct330@gmail.com>
2024-09-09 10:44:00 +0700

    Search by brand

---
+++ Project_Demo/Controllers/BrandController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Project_Demo.Models;
+using Project_Demo.Repository;
+
+namespace Project_Demo.Controllers
+{
+	public class BrandController : Controller
+	{
+		public readonly DataContext _dataContext;
+		public BrandController(DataContext dataContext)
+		{
+			_dataContext = dataContext;
+		}
+		public async Task<IActionResult> Index(string Slug = "")
+		{
+			BrandModel brand = _dataContext.Brands.Where(c => c.Slug == Slug).FirstOrDefault();
+			if (brand == null) return RedirectToAction("Index");
+			var productsByBrand = _dataContext.Products.Where(p => p.BrandID == brand.Id);
+			return View(await productsByBrand.OrderByDescending(p => p.Id).ToListAsync());
+		}
+	}
+}

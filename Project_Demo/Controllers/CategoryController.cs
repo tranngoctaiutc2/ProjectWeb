0c03e2652e1cfab0e1da9a378e9ebdf5378fbcdd
tranngoctaiutc2 <tranngoct330@gmail.com>
2024-09-09 10:28:37 +0700

    Search by category

--- Project_Demo/Controllers/CategoryController.cs
+++ Project_Demo/Controllers/CategoryController.cs
@@ -1,12 +1,23 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Project_Demo.Models;
+using Project_Demo.Repository;
 
 namespace Project_Demo.Controllers
 {
     public class CategoryController : Controller
     {
-        public IActionResult Index()
+        public readonly DataContext _dataContext;
+        public CategoryController(DataContext dataContext)
         {
-            return View();
+            _dataContext = dataContext;
+        }
+        public async Task<IActionResult> Index(string Slug = "")
+        {
+            CategoryModel category = _dataContext.Categories.Where(c => c.Slug == Slug).FirstOrDefault();
+            if (category == null) return RedirectToAction("Index");
+            var productsByCategory = _dataContext.Products.Where(p => p.CategoryID == category.Id);
+            return View(await productsByCategory.OrderByDescending(p => p.Id).ToListAsync());
         }
     }
 }

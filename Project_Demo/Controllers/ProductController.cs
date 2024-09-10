21d3197425ffd13d227fc1d1b4ddaa275cde2efa
tranngoctaiutc2 <tranngoct330@gmail.com>
2024-09-10 13:30:41 +0700

    Add details product

--- Project_Demo/Controllers/ProductController.cs
+++ Project_Demo/Controllers/ProductController.cs
@@ -1,17 +1,25 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Project_Demo.Repository;
 
 namespace Project_Demo.Controllers
 {
     public class ProductController : Controller
     {
+        private readonly DataContext _dataContext;
+        public ProductController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult Details()
+        public async Task<IActionResult> Details(int Id)
         {
-            return View();
+            if (Id == null) return RedirectToAction("Index");
+            var productsById = _dataContext.Products.Where(p => p.Id == Id).FirstOrDefault();
+            return View(productsById);
         }
 
     }

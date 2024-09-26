c86f1c17e50b302784d32ab39cd233529381a8a4
tranngoctaiutc2 <tranngoct330@gmail.com>
2024-09-26 09:27:59 +0700

    Add Brand Administration

---
+++ Project_Demo/Areas/Admin/Controllers/BrandController.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Project_Demo.Models;
+using Project_Demo.Repository;
+
+namespace Project_Demo.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class BrandController : Controller
+    {
+        private readonly DataContext _dataContext;
+        public BrandController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+        public async Task<IActionResult> Index()
+        {
+            return View(await _dataContext.Brands.OrderByDescending(p => p.Id).ToListAsync());
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(BrandModel brand)
+        {
+            if (ModelState.IsValid)
+            {
+                brand.Slug = brand.Name.Replace(" ", "-");
+                var slug = await _dataContext.Categories.FirstOrDefaultAsync(c => c.Slug == brand.Slug);
+                if (slug != null)
+                {
+                    ModelState.AddModelError("", "Thương hiệu đã tồn tại");
+                    return View(brand);
+                }
+                _dataContext.Add(brand);
+                await _dataContext.SaveChangesAsync();
+                TempData["succes"] = "Thêm thương hiệu thành công";
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                TempData["error"] = "Model lỗi";
+                List<String> errors = new List<String>();
+                foreach (var value in ModelState.Values)
+                {
+                    foreach (var error in value.Errors)
+                    {
+                        errors.Add(error.ErrorMessage);
+                    }
+                }
+                string errorMessage = string.Join("\n", errors);
+                return BadRequest(errorMessage);
+            }
+            return View(brand);
+        }
+        public async Task<IActionResult> Delete(int Id)
+        {
+            BrandModel brand = await _dataContext.Brands.FindAsync(Id);
+            _dataContext.Brands.Remove(brand);
+            await _dataContext.SaveChangesAsync();
+            TempData["succes"] = "Thương hiệu đã xoá thành công";
+            return RedirectToAction("Index");
+        }
+        public async Task<IActionResult> Edit(int Id)
+        {
+            BrandModel brand = await _dataContext.Brands.FindAsync(Id);
+            return View(brand);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(BrandModel brand)
+        {
+            if (ModelState.IsValid)
+            {
+                brand.Slug = brand.Name.Replace(" ", "-");
+                var slug = await _dataContext.Brands.FirstOrDefaultAsync(c => c.Slug == brand.Slug);
+                if (slug != null)
+                {
+                    ModelState.AddModelError("", "Danh mục đã tồn tại");
+                    return View(brand);
+                }
+                _dataContext.Update(brand);
+                await _dataContext.SaveChangesAsync();
+                TempData["succes"] = "Cập nhật danh mục thành công";
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                TempData["error"] = "Model lỗi";
+                List<String> errors = new List<String>();
+                foreach (var value in ModelState.Values)
+                {
+                    foreach (var error in value.Errors)
+                    {
+                        errors.Add(error.ErrorMessage);
+                    }
+                }
+                string errorMessage = string.Join("\n", errors);
+                return BadRequest(errorMessage);
+            }
+            return View(brand);
+        }
+    }
+}
--- Project_Demo/Models/BrandModel.cs
+++ Project_Demo/Models/BrandModel.cs
@@ -6,11 +6,10 @@ namespace Project_Demo.Models
     {
         [Key]
         public int Id { get; set; }
-        [Required, MinLength(4, ErrorMessage = "Tên thương hiệu không được để trống")]
+        [Required(ErrorMessage = "Tên thương hiệu không được để trống")]
         public string Name { get; set; }
-        [Required, MinLength(4, ErrorMessage = "Mô tả thương hiệu không được để trống")]
+        [Required(ErrorMessage = "Mô tả thương hiệu không được để trống")]
         public string Description { get; set; }
-        [Required]
         public string Slug { get; set; }
         public int Status { get; set; }
     }

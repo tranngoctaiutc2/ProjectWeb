ebe61bd6532533c3a6c88699e7a4ec87ad64c523
tranngoctaiutc2 <tranngoct330@gmail.com>
2024-09-27 12:20:24 +0700

    Add User Login

--- Project_Demo/Controllers/AccountController.cs
+++ Project_Demo/Controllers/AccountController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Project_Demo.Models;
+using Project_Demo.Models.ViewModels;
 
 namespace Project_Demo.Controllers
 {
@@ -15,30 +16,39 @@ namespace Project_Demo.Controllers
             _userManager = userManager;
         }
 
-		public IActionResult Index()
+		public IActionResult Login(string returnUrl)
         {
+            return View(new LoginViewModel { ReturnUrl=returnUrl});
+        }
+		[HttpPost]
+		public async Task<IActionResult> Login(LoginViewModel LoginVM)
+        {
+            if (ModelState.IsValid)
+            {
+                Microsoft.AspNetCore.Identity.SignInResult result = await _signInManager.PasswordSignInAsync(LoginVM.UserName, LoginVM.Password,false,false);
+                if (result.Succeeded)
+                {
+                    return Redirect(LoginVM.ReturnUrl ?? "/");
+                }
+                ModelState.AddModelError("", "Sai tên đăng nhập hoặc mật khẩu ");
+            }
             return View();
         }
 		public IActionResult Create()
 		{
 			return View();
 		}
-		public async Task<IActionResult> Login()
-        {
-            return View();
-        }
         [HttpPost]
-        [ValidateAntiForgeryToken]
 		public async Task<IActionResult> Create(UserModel user)
 		{
             if (ModelState.IsValid)
             {
                 AppUserModel newUser = new AppUserModel { UserName = user.UserName, Email = user.Email};
-                IdentityResult result = await _userManager.CreateAsync(newUser);
+                IdentityResult result = await _userManager.CreateAsync(newUser, user.Password);
                 if(result.Succeeded)
                 {
                     TempData["succes"] = "Tạo thành công user";
-                    return Redirect("/account");
+                    return Redirect("/account/login");
                 }
                 foreach(IdentityError error in result.Errors)
                 {
---
+++ Project_Demo/Models/ViewModels/LoginViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Project_Demo.Models.ViewModels
+{
+	public class LoginViewModel
+	{
+		public int Id { get; set; }
+		[Required(ErrorMessage = "Hãy nhập tên đăng nhập")]
+		public string UserName { get; set; }
+		[DataType(DataType.Password), Required(ErrorMessage = "Hãy nhập mật khẩu")]
+		public string Password { get; set; }
+		public string ReturnUrl { get; set; }
+	}
+}
